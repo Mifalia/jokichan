9ac5033d2a3f710937db47379262c48e20fde808
mifalia <mifarivonjisoa@gmail.com>
2024-02-23 22:51:36 +0300

    fix : solve theme mode consistency on after refresh

--- src/store/ThemeStore.ts
+++ src/store/ThemeStore.ts
@@ -3,16 +3,22 @@ import { makeAutoObservable } from 'mobx';
 export type ThemeMode = 'dark' | 'light';
 
 class ThemeStore {
-  theme: ThemeMode = 'light';
+  theme: ThemeMode = localStorage.getItem('theme-mode') === 'dark' ? 'dark' : 'light';
 
   constructor() {
+    this.setup();
     makeAutoObservable(this);
+  }
+
+  setup() {
+    document.documentElement.classList.add(this.theme);
   }
 
   toggleTheme() {
     document.documentElement.classList.remove(this.theme);
     this.theme = this.theme === 'light' ? 'dark' : 'light';
     document.documentElement.classList.add(this.theme);
+    localStorage.setItem('theme-mode', this.theme);
   }
 }
 
