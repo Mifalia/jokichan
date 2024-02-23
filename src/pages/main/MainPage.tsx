6af66b7f8522f9555d191ab62e4160497f2347c9
mifalia <mifarivonjisoa@gmail.com>
2024-02-23 15:23:23 +0300

    feat : add copy feature with toaster notification

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -6,10 +6,33 @@ import { MdOutlineContentCopy } from 'react-icons/md';
 import { MdFilterList } from 'react-icons/md';
 import jokeStore from 'src/store/JokeStore';
 import { observer } from 'mobx-react';
+import toast, { Toaster } from 'react-hot-toast';
 
 function MainPage() {
   const handleGenerateClick = () => {
     jokeStore.generateJoke();
+  };
+
+  const handleCopy: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.stopPropagation();
+
+    if (!jokeStore.joke.error) {
+      let jokeToCopy: string | undefined = '';
+      switch (jokeStore.joke.type) {
+        case 'single':
+          jokeToCopy = jokeStore.joke.joke;
+          break;
+        case 'twopart':
+          jokeToCopy = `${jokeStore.joke.setup} ${jokeStore.joke.delivery}`;
+          break;
+        default:
+          break;
+      }
+      navigator.clipboard.writeText(`${jokeToCopy}`); //copy process
+      toast.success(`joke copied successfully`, { position: 'top-center' });
+
+      return;
+    }
   };
 
   const handleTagClick = (categoryName: string) => {
@@ -20,57 +43,64 @@ function MainPage() {
     RENDERING 
   ----*/
   return (
-    <div id='layout' className='app-container h-screen flex flex-col justify-center text-content'>
-      {/* main frame */}
-      <div id='main' className='h-full md:h-fit bg-fill rounded-none md:rounded-lg flex flex-col'>
-        {/* header */}
-        <Header />
-        {/* body */}
-        <div className='p-4'>
-          <p className='bg-surface font-mono rounded p-4 min-h-20'>
-            {jokeStore.isLoading ? (
-              <br />
-            ) : jokeStore.joke.type === 'single' ? (
-              jokeStore.joke.joke
-            ) : (
-              <>
-                {jokeStore.joke.setup}
+    <>
+      <div id='layout' className='app-container h-screen flex flex-col justify-center text-content'>
+        {/* main frame */}
+        <div id='main' className='h-full md:h-fit bg-fill rounded-none md:rounded-lg flex flex-col'>
+          {/* header */}
+          <Header />
+          {/* body */}
+          <div className='p-4'>
+            <p className='bg-surface font-mono rounded p-4 min-h-20'>
+              {jokeStore.isLoading ? (
                 <br />
-                {jokeStore.joke.delivery}
-              </>
-            )}
-          </p>
-          <div className='mt-4 p-0 flex flex-row justify-center items-center gap-3'>
-            <button className='button button-icon-start button-default shrink-0 justify-center grow md:grow-0'>
-              <MdOutlineContentCopy size={16} />
-              copy
-            </button>
-            <button className='button button-icon-start button-primary shrink-0 justify-center grow md:grow-0' onClick={handleGenerateClick}>
-              <MdLoop size={16} />
-              another one
-            </button>
-          </div>
-          <div className='mt-12 md:mt-8 flex flex-col md:flex-row justify-start items-start gap-4'>
-            <div className='flex flex-start items-center gap-1.5 font-extralight p-0 w-fit capitalize shrink-0 grow-0'>
-              <MdFilterList size={16} />
-              customize filters
+              ) : jokeStore.joke.type === 'single' ? (
+                jokeStore.joke.joke
+              ) : (
+                <>
+                  {jokeStore.joke.setup}
+                  <br />
+                  {jokeStore.joke.delivery}
+                </>
+              )}
+            </p>
+            <div className='mt-4 p-0 flex flex-row justify-center items-center gap-3'>
+              <button
+                className={`button button-icon-start button-default shrink-0 justify-center grow md:grow-0 ${jokeStore.isLoading ? 'cursor-not-allowed' : 'cursor-pointer'}`}
+                disabled={jokeStore.isLoading ? true : false}
+                onClick={handleCopy}
+              >
+                <MdOutlineContentCopy size={16} />
+                copy
+              </button>
+              <button className='button button-icon-start button-primary shrink-0 justify-center grow md:grow-0' onClick={handleGenerateClick}>
+                <MdLoop size={16} />
+                another one
+              </button>
             </div>
-            <div className='flex flex-start flex-wrap gap-x-2 gap-y-4'>
-              {jokeStore.categories?.map((category, index) => (
-                <div key={index} className='w-fit h-fit'>
-                  <input type='checkbox' id={category.name + 'Checkbox'} name={category.name} value={category.name} className='hidden' />
-                  <label htmlFor={category.name + 'Checkbox'} className={`cursor-pointer tag ${category.active ? 'active' : ''}`} onClick={() => handleTagClick(category?.name)}>
-                    {category.name}
-                  </label>
-                </div>
-              ))}
+            <div className='mt-12 md:mt-8 flex flex-col md:flex-row justify-start items-start gap-4'>
+              <div className='flex flex-start items-center gap-1.5 font-extralight p-0 w-fit capitalize shrink-0 grow-0'>
+                <MdFilterList size={16} />
+                customize filters
+              </div>
+              <div className='flex flex-start flex-wrap gap-x-2 gap-y-4'>
+                {jokeStore.categories?.map((category, index) => (
+                  <div key={index} className='w-fit h-fit'>
+                    <input type='checkbox' id={category.name + 'Checkbox'} name={category.name} value={category.name} className='hidden' />
+                    <label htmlFor={category.name + 'Checkbox'} className={`cursor-pointer tag ${category.active ? 'active' : ''}`} onClick={() => handleTagClick(category?.name)}>
+                      {category.name}
+                    </label>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
+          {/* footer */}
+          <Footer />
         </div>
-        {/* footer */}
-        <Footer />
       </div>
-    </div>
+      <Toaster />
+    </>
   );
 }
 
