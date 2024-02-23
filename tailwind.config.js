0a7bd62b9b727a8eeb00cdc4d462aad10468e4e7
mifalia <mifarivonjisoa@gmail.com>
2024-02-23 13:56:33 +0300

    feat : add button opacity animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
         surface: 'var(--surface)',
         fill: 'var(--fill)',
         border: 'var(--border)',
+        darken: 'var(--darken)',
         'global-layout-fill': 'var(--global-layout-fill)',
       },
     },
